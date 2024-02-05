1001dc2d53504b3484969f79c0105f90ab6590c1
Umar Muhammad Aminu <khalifaumar308@gmail.com>
2024-02-05 14:31:50 +0100

    Update backend

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -16,7 +16,7 @@ import { setCredentials, logOut } from "../middleware/auth/authSlice";
 
 // export const { useSaveStudentMutation } = appApi;
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://ntmbackend.onrender.com",
+  baseUrl: "https://northfield-montessori.onrender.com",
   // baseUrl: "http://localhost:3000",
   credentials: "same-origin",
   // mode: "no-cors",
